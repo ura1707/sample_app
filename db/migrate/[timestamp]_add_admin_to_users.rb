0e566b88f6cecaef5c09bdd7fe324202f8981122
Hiroki <ura-sparkingdays@jcom.home.ne.jp>
2017-08-23 13:57:45 +0000

    Make a Micropost model

--- db/migrate/[timestamp]_add_admin_to_users.rb
+++ db/migrate/[timestamp]_add_admin_to_users.rb
@@ -1,5 +1,12 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :admin, :boolean, default: false
+   create_table :microposts do |t|
+      t.string :content
+      t.integer :user_id
+
+
+      t.timestamps
+    end
+    add_index :microposts, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820160108) do
+ActiveRecord::Schema.define(version: 20170822162803) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
